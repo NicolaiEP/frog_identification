b1b9418e3a1449404779bf7784ba165ab2db68fc
NicolaiEP <nicolaiep@uia.no>
2023-05-15 13:48:13 +0200

    Delete

--- utils/data_utils.py
+++
@@ -1,7 +0,0 @@
-from detectron2.data import MetadataCatalog
-from detectron2.data.datasets import register_coco_instances
-
-
-def register_frog_dataset(name, images_dir, json_file):
-    register_coco_instances(name, {}, json_file, images_dir)
-    MetadataCatalog.get(name).set(thing_classes=["frog_stomach"])
